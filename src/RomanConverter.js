60e4af3f50d5b15203e18363e410137ad26a2566
leonardocardoso <leonardo.t.cardoso@slock.it>
2021-04-07 13:58:14 +0200

    Implement fromRoman method

--- src/RomanConverter.js
+++ src/RomanConverter.js
@@ -1,61 +1,60 @@
 import _ from 'lodash'
 
-const doubleDigit = {
-    CM: 900,
-    CD: 400,
-    XC: 90,
-    XL: 40,
-    IX: 9,
-    IV: 4
-}
-
 const singleDigit = {
-    M: 1000,
-    D: 500,
-    C: 100,
-    L: 50,
-    X: 10,
-    V: 5,
-    I: 10
+  M: 1000,
+  D: 500,
+  C: 100,
+  L: 50,
+  X: 10,
+  V: 5,
+  I: 1
 }
 
 const digits = {
-    M: { value: 1000, isSingleDigit: true },
-    CM: { value: 900, isSingleDigit: false },
-    D: { value: 500, isSingleDigit: true },
-    CD: { value: 400, isSingleDigit: false },
-    C: { value: 100, isSingleDigit: true },
-    XC: { value: 90, isSingleDigit: false },
-    L: { value: 50, isSingleDigit: true },
-    XL: { value: 40, isSingleDigit: false },
-    X: { value: 10, isSingleDigit: true },
-    IX: { value: 9, isSingleDigit: false },
-    V: { value: 5, isSingleDigit: true },
-    IV: { value: 4, isSingleDigit: false },
-    I: { value: 1, isSingleDigit: true }
+  M: { value: 1000, isSingleDigit: true },
+  CM: { value: 900, isSingleDigit: false },
+  D: { value: 500, isSingleDigit: true },
+  CD: { value: 400, isSingleDigit: false },
+  C: { value: 100, isSingleDigit: true },
+  XC: { value: 90, isSingleDigit: false },
+  L: { value: 50, isSingleDigit: true },
+  XL: { value: 40, isSingleDigit: false },
+  X: { value: 10, isSingleDigit: true },
+  IX: { value: 9, isSingleDigit: false },
+  V: { value: 5, isSingleDigit: true },
+  IV: { value: 4, isSingleDigit: false },
+  I: { value: 1, isSingleDigit: true }
 }
 
 class RomanConverter {
-    fromRoman(romanStr) {
-        // TODO
+  static fromRoman (romanStr) {
+    let arabic = 0
+    const roman = romanStr.split('')
+    for (let i = 0; i < roman.length; i++) {
+      const next = roman[i + 1]
+      const arabicDigit = singleDigit[roman[i]]
+      arabic += !next || singleDigit[next] <= arabicDigit ? arabicDigit : arabicDigit * -1
     }
-    static toRoman(arabicStr) {
-        let romanStr = ""
-        let arabic = parseInt(arabicStr)
+    return arabic
+  }
 
-        _.each(digits, (algarism, romanLetters) => {
-            if (algarism.isSingleDigit) {
-                const digitCount = Math.floor(arabic / algarism.value)
-                arabic = arabic % algarism.value
-                romanStr += romanLetters.repeat(digitCount)
-            } else if (arabic >= algarism.value) {
-                arabic -= algarism.value
-                romanStr += romanLetters
-            }
-        })
+  static toRoman (arabicStr) {
+    let romanStr = ''
+    let arabic = parseInt(arabicStr)
 
-        return romanStr
-    }
+    _.each(digits, (algarism, romanLetters) => {
+      if (algarism.isSingleDigit) {
+        const digitCount = Math.floor(arabic / algarism.value)
+        arabic = arabic % algarism.value
+        romanStr += romanLetters.repeat(digitCount)
+      } else if (arabic >= algarism.value) {
+        arabic -= algarism.value
+        romanStr += romanLetters
+      }
+    })
+
+    return romanStr
+  }
 }
 
 export default RomanConverter
--- src/RomanConverter.test.js
+++ src/RomanConverter.test.js
@@ -1,20 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import RomanConverter from './RomanConverter';
+import { render, screen } from '@testing-library/react'
+import RomanConverter from './RomanConverter'
 
 test('converts arabic into roman', () => {
-  expect(RomanConverter.toRoman(1000)).toBe("M");
-  expect(RomanConverter.toRoman(39)).toBe("XXXIX");
-  expect(RomanConverter.toRoman(246)).toBe("CCXLVI");
-  expect(RomanConverter.toRoman(789)).toBe("DCCLXXXIX");
-  expect(RomanConverter.toRoman(2421)).toBe("MMCDXXI");
-  expect(RomanConverter.toRoman(0)).toBe("");
-});
+  expect(RomanConverter.toRoman(1000)).toBe('M')
+  expect(RomanConverter.toRoman(39)).toBe('XXXIX')
+  expect(RomanConverter.toRoman(246)).toBe('CCXLVI')
+  expect(RomanConverter.toRoman(789)).toBe('DCCLXXXIX')
+  expect(RomanConverter.toRoman(2421)).toBe('MMCDXXI')
+  expect(RomanConverter.toRoman(0)).toBe('')
+})
 
 test('converts roman into arabic', () => {
-    expect(RomanConverter.fromRoman("M")).toBe(1000);
-    expect(RomanConverter.fromRoman("XXXIX")).toBe(39);
-    expect(RomanConverter.fromRoman("CCXLVI")).toBe(246);
-    expect(RomanConverter.fromRoman("DCCLXXXIX")).toBe(789);
-    expect(RomanConverter.fromRoman("MMCDXXI")).toBe(2421);
-    expect(RomanConverter.fromRoman("")).toBe(0)
-});
+  expect(RomanConverter.fromRoman('M')).toBe(1000)
+  expect(RomanConverter.fromRoman('XXXIX')).toBe(39)
+  expect(RomanConverter.fromRoman('CCXLVI')).toBe(246)
+  expect(RomanConverter.fromRoman('DCCLXXXIX')).toBe(789)
+  expect(RomanConverter.fromRoman('MMCDXXI')).toBe(2421)
+  expect(RomanConverter.fromRoman('')).toBe(0)
+})
